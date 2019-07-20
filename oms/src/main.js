b7c4dbfb92a9303a4bf7354fafc17d982bb6aa5d
sunweixing123 <359561000@qq.com>
2019-07-20 09:28:22 +0800

    添加element

--- oms/src/main.js
+++ oms/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Button, Select } from 'element-ui';
+import './plugins/element.js'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Button);
+Vue.use(Select);
 
 new Vue({
   render: h => h(App),
